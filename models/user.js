8e0d593af9da42f11b9f1a3abd50f1c100a5713b
unknown <thecbradshaw89@gmail.com>
2023-02-08 18:58:21 -0600

    added .env, .gitignore, added connection.js, added server.js, added index for models with assocations, added animal_review model, added user model, added comment model

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class User extends Model {}
+//add password hashing with bcrypt
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allownull: false,
+            unique: true //unique
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5], //must be at least 5 characters
+            },
+        },
+
+
+
+    
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
